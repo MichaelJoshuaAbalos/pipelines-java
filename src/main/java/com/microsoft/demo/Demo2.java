1b28cc84e12b66ffb52d36170609a353ddfbd80f
Michael Joshua Abalos <michaeljoshua.abalos@fujitsu.com>
2023-03-20 10:28:07 +0800

    Added Scanner code

--- src/main/java/com/microsoft/demo/Demo2.java
+++ src/main/java/com/microsoft/demo/Demo2.java
@@ -1,8 +1,15 @@
 package com.microsoft.demo;
 
+import java.util.Scanner;
+
 public class Demo2 {
     public static void main(String[] args) {
         System.out.println("This is another file");
+        
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Input a message");
+        String message = sc.nextLine();
+        System.out.println(message);
     }
     
 }
